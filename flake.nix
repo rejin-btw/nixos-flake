af45de93c6757fefdda6db6e1c484f3f8431fcd7
rejin-btw <rejinks@zohomail.in>
2025-11-24 12:26:07 +0530

    chekc

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
-    dotfiles.url = "github:rejin-btw/dotfiles";
+    
+    # --- THE FIX ---
+    # OLD: dotfiles.url = "github:rejin-btw/dotfiles";
+    # NEW: Point to your local repository on disk
+    dotfiles.url = "git+file:///home/rejin/dotfiles";
   };
 
   outputs = { self, nixpkgs, home-manager, dotfiles, ... }:
   let
     system = "x86_64-linux";
+    # Now this path will come from your local disk, not the internet
     dotfilesPath = "${dotfiles}/scripts";
   in {
     nixosConfigurations = {
@@ -42,4 +47,3 @@
     };
   };
 }
-
--- home/rejin.nix
+++ home/rejin.nix
@@ -1,119 +1,101 @@
 { config, pkgs, lib, dotfilesPath, ... }:
 
 let
-  scriptsDir = pkgs.runCommand "scripts" {
-    src = "${dotfilesPath}";
-  } ''
-    mkdir -p $out
-    cp -r $src $out
-  '';
+  # --- CONFIGURATION ---
+  # The absolute path to your git repo on disk
+  localDotfiles = "/home/rejin/dotfiles";
+
+  # --- HELPERS ---
+  # Helper to link .config folders (e.g., ~/.config/mako -> ~/dotfiles/.config/mako)
+  mkLink = path: config.lib.file.mkOutOfStoreSymlink "${localDotfiles}/.config/${path}";
+
+  # Helper to link folders in Home Root (e.g., ~/scripts -> ~/dotfiles/scripts)
+  mkLinkHome = path: config.lib.file.mkOutOfStoreSymlink "${localDotfiles}/${path}";
+
+  # List of standard config folders to loop over
+  simpleConfigs = [ 
+    "mako" 
+    "fuzzel" 
+    "zathura" 
+    "lf" 
+    "alacritty" 
+  ];
+
 in
 {
   home.username = "rejin";
   home.homeDirectory = "/home/rejin";
   home.stateVersion = "25.05";
 
+  # --- PACKAGES & WRAPPERS ---
   home.packages = with pkgs; [
-    vim
-    wget
-    fuzzel
-    mako
-    neovim
-    wl-clipboard
-    cliphist
-    wl-clip-persist
-    telegram-desktop
-    phinger-cursors
-    appflowy
-    tree
-    stow
-    pavucontrol
-    tmux
-    python3
-    inotify-tools
-    lhasa
-    zip
-    unzip
-    arj
-    unar
-    rpm
-    p7zip
-    freetube
-    zathura
-    gnome-themes-extra
-    adwaita-qt
-    xfce.thunar
-    xfce.thunar-volman
-    xfce.thunar-media-tags-plugin
-    xfce.thunar-archive-plugin
-    lf
-    fd
-    fzf
-    mpv
-    localsend
-    eza
-    bat
-    ripgrep
-    alacritty
-    starship
-    zoxide
-    
+    # GUI / CLI Tools
+    vim wget fuzzel mako neovim wl-clipboard cliphist wl-clip-persist
+    telegram-desktop phinger-cursors appflowy tree stow pavucontrol
+    tmux python3 inotify-tools lhasa zip unzip arj unar rpm p7zip
+    freetube zathura gnome-themes-extra adwaita-qt
+    xfce.thunar xfce.thunar-volman xfce.thunar-media-tags-plugin xfce.thunar-archive-plugin
+    lf fd fzf mpv localsend eza bat ripgrep alacritty starship zoxide
 
-    
+    # --- CUSTOM SCRIPTS (LIVE EDITING ENABLED) ---
+    # These wrappers set up the dependencies ($PATH) but execute the file 
+    # directly from your disk. Edit the file -> Run script -> Instant update.
 
-    # Custom scripts
     (writeShellScriptBin "auto-consume" ''
-      export PATH="${lib.makeBinPath [ libnotify jq procps niri coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/auto_consume.sh"}
+      export PATH="${lib.makeBinPath [ libnotify jq procps niri coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/auto_consume.sh "$@"
     '')
 
     (writeShellScriptBin "datetime-notify" ''
-      export PATH="${lib.makeBinPath [ libnotify coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/datetime_notify.sh"}
+      export PATH="${lib.makeBinPath [ libnotify coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/datetime_notify.sh "$@"
     '')
 
     (writeShellScriptBin "nvim-thunar" ''
-      export PATH="${lib.makeBinPath [ neovim coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/nvim_thunar.sh"}
+      export PATH="${lib.makeBinPath [ neovim coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/nvim_thunar.sh "$@"
     '')
 
     (writeShellScriptBin "ram-monitor" ''
-      export PATH="${lib.makeBinPath [ libnotify procps gawk coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/ram_monitor.sh"}
+      export PATH="${lib.makeBinPath [ libnotify procps gawk coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/ram_monitor.sh "$@"
     '')
 
     (writeShellScriptBin "toggle-audio" ''
-      export PATH="${lib.makeBinPath [ pkgs.pulseaudio pkgs.gnugrep pkgs.gawk pkgs.coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/toggle-audio.sh"}
+      export PATH="${lib.makeBinPath [ pkgs.pulseaudio pkgs.gnugrep pkgs.gawk pkgs.coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/toggle-audio.sh "$@"
     '')
 
     (writeShellScriptBin "vcp-control" ''
-      export PATH="${lib.makeBinPath [ ddcutil coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/vcp_full_control.sh"}
-    '')
-
-    (pkgs.writeScriptBin "niri-mouse-scroll" ''
-      #!${pkgs.python3.withPackages (ps: [ ps.evdev ])}/bin/python3
-      ${builtins.readFile "${scriptsDir}/scripts/niri-mouse-scroll.py"}
+      export PATH="${lib.makeBinPath [ ddcutil coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/vcp_full_control.sh "$@"
     '')
 
     (writeShellScriptBin "start-niri" ''
-      export PATH="${lib.makeBinPath [ niri coreutils ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/start-niri.sh"}
+      export PATH="${lib.makeBinPath [ niri coreutils ]}:$PATH"
+      exec ${localDotfiles}/scripts/start-niri.sh "$@"
     '')
-
-    (pkgs.writeScriptBin "fuzzel-bookmarks" ''
-      #!${pkgs.python3.withPackages (ps: [ ps.evdev ])}/bin/python3
-      ${builtins.readFile "${scriptsDir}/scripts/firefox-bookmarks-fuzzel.py"}
-    '')
-
 
     (writeShellScriptBin "watch-firefox-bookmarks" ''
-      export PATH="${lib.makeBinPath [ inotify-tools python3 ]}:$$PATH"
-      ${builtins.readFile "${scriptsDir}/scripts/bookmarks_watcher.sh"}
+      export PATH="${lib.makeBinPath [ inotify-tools python3 ]}:$PATH"
+      exec ${localDotfiles}/scripts/bookmarks_watcher.sh "$@"
+    '')
+
+    # --- PYTHON SCRIPTS ---
+    # We create a wrapper that includes the python env, then runs your local file.
+
+    (writeShellScriptBin "niri-mouse-scroll" ''
+      export PATH="${lib.makeBinPath [ (pkgs.python3.withPackages (ps: [ ps.evdev ])) ]}:$PATH"
+      exec python3 ${localDotfiles}/scripts/niri-mouse-scroll.py "$@"
+    '')
+
+    (writeShellScriptBin "fuzzel-bookmarks" ''
+      export PATH="${lib.makeBinPath [ (pkgs.python3.withPackages (ps: [ ps.evdev ])) ]}:$PATH"
+      exec python3 ${localDotfiles}/scripts/firefox-bookmarks-fuzzel.py "$@"
     '')
   ];
 
+  # --- GIT ---
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
@@ -125,68 +107,40 @@ in
     };
   };
 
-    programs.firefox.enable = true;
+  programs.firefox.enable = true;
 
-
-
-
-
-    
-
-
-    
-    
-
-   home.file = {
-   ".config/niri".source = "${dotfilesPath}/../.config/niri/nix";
-   ".config/mako".source = "${dotfilesPath}/../.config/mako";
-   ".config/fuzzel".source = "${dotfilesPath}/../.config/fuzzel";
-   ".config/zathura".source = "${dotfilesPath}/../.config/zathura";
-   ".config/lf".source = "${dotfilesPath}/../.config/lf";
-   ".config/alacritty".source= "${dotfilesPath}/../.config/alacritty";
-      "scripts".source = "${dotfilesPath}";
- };
-
+  # --- CONFIG MANAGEMENT (THE FIX) ---
   
+  # 1. Handle .config files
+  xdg.configFile = 
+    # Loop over the simple folders (recursive = false by default)
+    (lib.genAttrs simpleConfigs (name: { source = mkLink name; })) 
+    // 
+    # Add special cases (Niri points to a subfolder)
+    {
+      "niri".source = mkLink "niri/nix";
+    };
 
-    xdg.portal = {
-  enable = true;
-  extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-gnome
-  ];
-  config = {
-    common.default = "*";
+  # 2. Handle files in Home Root (Scripts folder)
+  home.file = {
+    "scripts".source = mkLinkHome "scripts";
   };
-};
 
-
-  
+  # --- THEME & INTEGRATION ---
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-gnome ];
+    config.common.default = "*";
+  };
 
   gtk = {
-  enable = true;
-  theme = {
-    name = "Adwaita-dark";
-    package = pkgs.gnome-themes-extra;
+    enable = true;
+    theme = { name = "Adwaita-dark"; package = pkgs.gnome-themes-extra; };
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
-  gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-  gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
-};
 
-qt = {
-  enable = true;
-  style.name = "adwaita-dark";
-};
+  qt = { enable = true; style.name = "adwaita-dark"; };
 
-
-  
-
-   
-  
-   home.sessionVariables = {
-     EDITOR = "vim";
-   };
+  home.sessionVariables = { EDITOR = "vim"; };
 }
-
-
-
